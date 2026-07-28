6f799cf7c65a13017d17fdd3b20a586d0104333b
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2026-07-28 12:06:10 +0800

    Web Dev Day 10 - Javascript

---
+++ 14.0 Javascript/index.js
@@ -0,0 +1,58 @@
+// alert("Hello");
+// alert("World");
+
+// console.log(1 + 2);
+// console.log(1 - 5);
+// console.log(2 * 3);
+// console.log(4 / 2);
+// console.log(typeof(23));
+// console.log(typeof("Khyle"));
+// console.log(typeof(true));
+
+// a = "Hello";
+// b = "World!";
+
+// console.log(a + " " + b);
+// console.log(a, b);
+// console.log(`${a} ${b}`);
+
+// const name = "Khyle"; //can't be changed
+// var name = "Khyle";
+
+// let name = "Khyle";
+// yourname = prompt("What is your name?");
+// console.log(`Hi I am ${name}, Hello ${yourname}!`);
+
+// var lvl = 1;
+// lvl++;
+// lvl+=2;
+// console.log("Your lvl is", lvl);
+
+// //Exercise 1 - change value of a to b and b to a
+// var a = "3";
+// var b = "8";
+// var c = a;
+// a = b;
+// b = c;
+// console.log("a is " + a);
+// console.log("b is " + b);
+
+// // naming
+// var myName = "Khyle";
+// var yourName = "Ria";
+// var myvar = "abc";
+// var my123 = 123;
+// var my_name = "Khyle";
+// var abc123_a = "abcd";
+// var userProfile = "./profile.png"
+
+// var word = "Exercise";
+// console.log(word.length)
+
+// Exercise 2 - Create a prompt where in it counts how many characters they input and how many characters remaining
+// max of 140 characters example: You have written 182 characters, you have -42 characters left.
+// // Code:
+// blog = prompt("Write a blog:")
+// console.log(`You have written ${blog.length}, you have ${ 140 - blog.length} left.`)
+// //140 character - //oadsjpoadjsopdas daopsjdasop sdaopdasj dasopjadsop dasopdjasopads dasopjadsop adsopdjasopdas adsopjadsopdas aadsopjopdas saodpjdasopdas asdo
+// //192 character - //oadsjpoadjsopdas daopsjdasop sdaopdasj dasopjadsop dasopdjasopads dasopjadsop adsopdjasopdas adsopjadsopdas aadsopjopdas saodpjdasopdas asdo dasbijadsbio asdioadshioads aidsooadsiaab
