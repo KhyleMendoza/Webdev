6abfd5ceee6555f72ad9f909b810b6f51e389227
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2026-07-29 15:43:39 +0800

    Web Dev Day 11 - Javascript - BMI Calculator

---
+++ 15.0 Javascript Intermediate/index.js
@@ -0,0 +1,18 @@
+//Random
+// var n = Math.random();
+// console.log(n);
+
+// n = n * 6;
+// console.log(n);
+// n = Math.floor(n) + 1;
+// console.log(n);
+
+// // Love Calculator - ask user 2 names and generate random number from 1-100
+// var firstPerson = prompt("What is your name?");
+// var secondPerson = prompt("What is your crush name?");
+
+// var rand = Math.random();
+// rand = rand * 100;
+// var lovePercent = Math.floor(rand) + 1;
+// //console.log(lovePercent)
+// alert(`${firstPerson} and ${secondPerson} is ${lovePercent}% in love!`)
