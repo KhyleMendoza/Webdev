2040eddb954fc6e516abba25f3d82b55f83669f3
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2026-07-29 16:03:46 +0800

    Web Dev Day 11 - Intermediate Javascript - Love Calculator

--- 15.0 Intermediate Javascript/index.js
+++ 15.0 Intermediate Javascript/index.js
@@ -7,12 +7,22 @@
 // n = Math.floor(n) + 1;
 // console.log(n);
 
-// // Love Calculator - ask user 2 names and generate random number from 1-100
-// var firstPerson = prompt("What is your name?");
-// var secondPerson = prompt("What is your crush name?");
-
-// var rand = Math.random();
-// rand = rand * 100;
-// var lovePercent = Math.floor(rand) + 1;
-// //console.log(lovePercent)
-// alert(`${firstPerson} and ${secondPerson} is ${lovePercent}% in love!`)
+// Love Calculator - ask user 2 names and generate random number from 1-100
+var firstPerson = prompt("What is your name?");
+var secondPerson = prompt("What is your crush name?");
+var rand = Math.random();
+rand = rand * 100;
+var lovePercent = Math.floor(rand) + 1;
+//console.log(lovePercent)
+alert(`${firstPerson} and ${secondPerson} is ${lovePercent}% in love!`)
+if (lovePercent <= 20) {
+    alert("Poor");
+} else if (lovePercent >= 21 && lovePercent <= 40) {
+    alert("Sad");
+} else if (lovePercent >= 41 && lovePercent <= 60) {
+    alert("Average")
+} else if (lovePercent >= 61 && lovePercent <= 80) {
+    alert("Good")
+} else if (lovePercent >= 81 && lovePercent <= 100) {
+    alert("Perfect")
+}
