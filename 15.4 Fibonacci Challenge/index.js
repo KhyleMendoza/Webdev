498a2cb94e1708b8e6b18095944a30961a2b8851
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2026-07-31 17:05:35 +0800

    Web Dev Day 13 - Javascript - Fibonacci Challenge

--- 15.4 Fibonacci Challenge/index.js
+++ 15.4 Fibonacci Challenge/index.js
@@ -1,22 +1,44 @@
 //V1:
+// function fibonacciGenerator(n) {
+//     var fibonacci = []
+//     var i = 0
+
+//     if (n >= 1) {
+//         fibonacci.push(i)
+//     }
+
+//     if (n >= 2) {
+//         i++;
+//         fibonacci.push(i)
+//     }
+
+//     for (var i = 1; i < n-1; i++) {
+//         var fibo = fibonacci[i] + fibonacci[i - 1];
+//         fibonacci.push(fibo)
+//     }
+
+//     return fibonacci
+// }
+
+// output = fibonacciGenerator(5)
+// console.log(output)
+
+// V2:
 function fibonacciGenerator(n) {
-    fibonacci = []
-    var i = 0
+    var fibonacci = []
 
     if (n >= 1) {
-        fibonacci.push(i)
+        fibonacci.push(0);
     }
 
     if (n >= 2) {
-        i++;
-        fibonacci.push(i)
+        fibonacci.push(1);
     }
 
-    for (var i = 1; i < n-1; i++) {
-        var fibo = fibonacci[i] + fibonacci[i - 1];
-        fibonacci.push(fibo)
+    for (var i = 2; i < n; i++) {
+        var fibo = fibonacci[fibonacci.length - 2] + fibonacci[fibonacci.length - 1];
+        fibonacci.push(fibo);
     }
-
 
     return fibonacci
 }
