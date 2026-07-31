e397676f60d3691a61d60251199112f359891859
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2026-07-31 16:08:50 +0800

    Web Dev Day 13 - Javascript - 99 Bottles Lyrics Challenge - while loop

--- 15.3 99 Bottles Lyrics Challenge/index.js
+++ 15.3 99 Bottles Lyrics Challenge/index.js
@@ -3,20 +3,20 @@ function bottlesOfBeerLyrics() {
 
     console.log("Lyrics of the song 99 Bottles of Beer")
 
-    while (bottles > 0) {
+    while (bottles >= 0) {
         if (bottles > 1) {
             console.log(`${bottles} bottles of beer on the wall, ${bottles} of beer.`)
             bottles--;
             console.log(`Take one down and pass it around, ${bottles} bottles of beer on the wall.`)
-        } else {
+        } else if (bottle = 1) {
             console.log(`${bottles} bottles of beer on the wall, ${bottles} of beer.`)
             bottles--;
             console.log(`Take one down and pass it around, no more bottles of beer on the wall.`)
+        } else {
+            console.log(`no more bottles of beer on the wall, no more bottles of beer.`)
+            console.log("Go to the store and buy some more, 99 bottles of beer on the wall.")
         }
     }
-
-    console.log(`no more bottles of beer on the wall, no more bottles of beer.`)
-    console.log("Go to the store and buy some more, 99 bottles of beer on the wall.")
 }
 
 bottlesOfBeerLyrics()
