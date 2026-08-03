6c674e0909be61672aba482f10ee80610681073b
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2026-08-03 15:17:36 +0800

    Web Dev Day 16 - Advanced Javascript - Constructor Function

--- 15.0 Intermediate Javascript/index.js
+++ 15.0 Intermediate Javascript/index.js
@@ -102,25 +102,76 @@
 
 // V2
 
-function add(num1, num2) {
-    return num1 + num2;
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
+
+// function sub(num1, num2) {
+//     return num1 - num2;
+// }
+
+// function mul(num1, num2) {
+//     return num1 * num2;
+// }
+
+// function div(num1, num2) {
+//     return num1 / num2;
+// }
+
+// function calculator(num1, num2, operator) {
+//     return operator(num1, num2)
+// }
+
+// answer = calculator(15, 6, sub)
+// console.log(answer)
+
+// Javascript Object
+// var bellboy1 = {
+//     name: "timmy",
+//     age: 21,
+//     hasWorkPermit: true,
+//     languages: ["French", "English"]
+// }
+// console.log(`Hello, my name is ${bellboy1.name}, my age is ${bellboy1.age} and i speak ${bellboy1.languages[1]} and ${bellboy1.languages[0]}!`)
+
+// let houseKeeper1 = {
+//     name: "naura",
+//     age: 27,
+//     gender: "female",
+//     isAssigned: true,
+//     task: ["clean the room", "clean the living room", "cook meal"]
+// }
+
+// if (houseKeeper1.isAssigned == true) {
+//     console.log(`Hello, I am ${houseKeeper1.name} and im your new house keeper. my age is ${houseKeeper1.age} and im currently assigned to these task:`)
+//     for (i = 0; i < houseKeeper1.task.length; i++) {
+//         console.log(houseKeeper1.task[i])
+//     }
+// } else {
+//     console.log(`Hello, I am ${houseKeeper1.name} and im your new house keeper. my age is ${houseKeeper1.age} and im currently not assigned to task.`)
+// }
+
+// Constructor Function - capital first letter of word
+function BellBoy (name, age, hasWorkPermit, languages) {
+    this.name = name;
+    this.age = age;
+    this.hasWorkPermit = hasWorkPermit;
+    this.languages = languages;
 }
 
-function sub(num1, num2) {
-    return num1 - num2;
+var bellboy1 = new BellBoy("John", 23, true, ["English, Japanase"]);
+var bellboy2 = new BellBoy("Carl", 22, false, ["English"]);
+var bellboy3 = new BellBoy("Nick", 25, true, ["English"]);
+console.log(bellboy1)
+console.log("Hi i am " + bellboy1.name + " and This is bellboy2: " + bellboy2.name + " and the other one is " + bellboy3.name);
+
+function HouseKeeper(yearsOfExperience, name, cleaningRepertoire) {
+    this.yearsOfExperience = yearsOfExperience;
+    this.name = name;
+    this.cleaningRepertoire = cleaningRepertoire;
 }
 
-function mul(num1, num2) {
-    return num1 * num2;
-}
-
-function div(num1, num2) {
-    return num1 / num2;
-}
-
-function calculator(num1, num2, operator) {
-    return operator(num1, num2)
-}
-
-answer = calculator(15, 6, sub)
-console.log(answer)
+var houseKeeper1 = new HouseKeeper(5, "Jane", ["Bathroom"]);
+var houseKeeper2 = new HouseKeeper(7, "norn", ["Livingroom", "Bathroom", "Kitchen"])
+console.log(houseKeeper1)
+console.log(houseKeeper2)
