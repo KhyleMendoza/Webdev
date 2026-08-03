2e98a1a5ac4154996b610d920705eadf53c08999
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2026-08-03 17:05:17 +0800

    Web Dev Day 16 - Drum Kit - using addEventListener, function, constructor function, key, this, switch, audio, play

--- 18.0 Drum Kit/index.js
+++ 18.0 Drum Kit/index.js
@@ -18,47 +18,70 @@
 
 let btn = document.querySelectorAll(".drum");
 
+function DrumSound(fileLocation) {
+    this.fileLocation = fileLocation;
+    this.play = function() {
+        var music = new Audio(this.fileLocation);
+        music.play();
+    }  
+}
+
+function playDrum(key) {
+    switch(key) {
+        case "w":
+            // let tom1 = new Audio("./sounds/tom-1.mp3")
+            // tom1.play();
+            let tom1 = new DrumSound("./sounds/tom-1.mp3")
+            tom1.play()
+            break;
+        case "a":
+            let tom2 = new DrumSound("./sounds/tom-2.mp3")
+            tom2.play()
+            break;
+        case "s":
+            let tom3 = new DrumSound("./sounds/tom-3.mp3")
+            tom3.play()
+            break;
+        case "d":
+            let tom4 = new DrumSound("./sounds/tom-4.mp3")
+            tom4.play();
+            break;
+        case "j":
+            let snare = new DrumSound("./sounds/snare.mp3")
+            snare.play();
+            break;
+        case "k":
+            let crash = new DrumSound("./sounds/crash.mp3")
+            crash.play();
+            break;
+        case "l":
+            let kickBass = new DrumSound("./sounds/kick-bass.mp3")
+            kickBass.play();
+            break;
+        default:
+            console.log(drumBtn)
+            return;
+    }
+}
+
 for (var i = 0; i < btn.length; i++) {
     btn[i].addEventListener("click", function() {
         // alert("I got clicked!");
         // console.log(this.innerText)
         // this.style.color = "white"
         // tom1.play();
-        var drumBtn = this.innerText;
-        switch(drumBtn) {
-            case "w":
-                let tom1 = new Audio("./sounds/tom-1.mp3")
-                tom1.play();
-                break;
-            case "a":
-                let tom2 = new Audio("./sounds/tom-2.mp3")
-                tom2.play();
-                break;
-            case "s":
-                let tom3 = new Audio("./sounds/tom-3.mp3")
-                tom3.play();
-                break;
-            case "d":
-                let tom4 = new Audio("./sounds/tom-4.mp3")
-                tom4.play();
-                break;
-            case "j":
-                let snare = new Audio("./sounds/snare.mp3")
-                snare.play();
-                break;
-            case "k":
-                let crash = new Audio("./sounds/crash.mp3")
-                crash.play();
-                break;
-            case "l":
-                let kickBass = new Audio("./sounds/kick-bass.mp3")
-                kickBass.play();
-                break;
-            default:
-                console.log(drumBtn)
-                return;
-        }
+
+        var key = this.innerText;
+        playDrum(key)
     });
 };
 
+// document.addEventListener("keydown", function(event) {
+//     console.log(event.key)
+// })
 
+document.addEventListener("keydown", e => {
+    // console.log(e.key + " Key was pressed!");
+    var key = e.key;
+    playDrum(key)
+})
