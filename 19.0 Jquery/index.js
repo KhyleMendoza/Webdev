791e8390d0a5a0a30c974957a2a56fd876453284
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2026-08-05 16:53:36 +0800

    Web Dev Day 18 - JQuery - add/remove class

--- 19.0 Jquery/index.js
+++ 19.0 Jquery/index.js
@@ -4,12 +4,21 @@
 //     $("h1").css("color", "white");
 // })
 
-$("h1").css("color", "white");
-$("h1").css("font-size", "50px");
-console.log($("h1").css("color"));
-console.log($("h1").css("font-size"));
+// $("h1").css("color", "white");
+// $("h1").css("font-size", "50px");
+// console.log($("h1").css("color"));
+// console.log($("h1").css("font-size"));
 
-$("button").css("backgroundColor", "#3440e9");
-$("button#red").css("color", "rgb(247, 24, 24)");
-$("button.blue").css("color", "#7b97d4");
-$("div button").css("color", "white");
+// $("button").css("backgroundColor", "#3440e9");
+// $("button#red").css("color", "rgb(247, 24, 24)");
+// $("button.blue").css("color", "#7b97d4");
+// $("div button").css("color", "white");
+
+// // add/remove class using jQuery
+// $("h1").addClass("big-title margin-50")
+// $("h1").removeClass("big-title margin-50")
+
+$("h1").addClass("big-title margin-50")
+console.log($("h1").hasClass("big-title"))
+console.log($("h1").hasClass("big-title margin-50"))
+console.log($("h1").hasClass("big-title margin-30"))
