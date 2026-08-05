7b98066ecfb236ac8ece5b1e9518015a6f25a672
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2026-08-05 16:41:26 +0800

    Web Dev Day 18 - JQuery

---
+++ 19.0 Jquery/index.js
@@ -0,0 +1,15 @@
+// jQuery("h1").css("color", "white")
+
+// $(document).ready(function() {
+//     $("h1").css("color", "white");
+// })
+
+$("h1").css("color", "white");
+$("h1").css("font-size", "50px");
+console.log($("h1").css("color"));
+console.log($("h1").css("font-size"));
+
+$("button").css("backgroundColor", "#3440e9");
+$("button#red").css("color", "rgb(247, 24, 24)");
+$("button.blue").css("color", "#7b97d4");
+$("div button").css("color", "white");
