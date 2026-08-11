86e795faede18891e4fa547c2b49a1cf0e52bd09
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2026-08-11 09:26:54 +0800

    Web Dev Day 24 - Node.js - create folders and organize files with fs module

---
+++ 23.4 Create folder and organize using Node/index.js
@@ -0,0 +1,30 @@
+import fs from 'fs'
+
+const folderName = ["img", "document", "video"]
+
+try {
+    for(var i=0; i < folderName.length; i++) {
+        if(!fs.existsSync(folderName[i])) {
+        fs.mkdirSync(folderName[i]);
+    }
+    }
+} catch (err) {
+    console.error(err);
+}
+
+// fs.renameSync("random/a.img", "img/a.txt")
+// fs.renameSync("random/b.docx", "document/b.txt")
+// fs.renameSync("random/c.txt", "video/c.txt")
+
+const files = fs.readdirSync("random");
+// console.log(files)
+let filepath = ["document", "img", "video"]
+
+for (var i = 0; i < files.length - 1; i++) {
+    if (!fs.existsSync(`${filepath[i]}/${files[i]}`)) {
+        fs.renameSync(`random/${files[i]}`, `${filepath[i]}/${files[i]}`)
+        console.log("done")
+    } else {
+        console.log("file not existing")
+    }
+}
