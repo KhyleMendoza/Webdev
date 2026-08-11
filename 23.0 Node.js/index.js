cb98cf45c767209aed1507e96c86d551f5847a76
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2026-08-11 11:42:18 +0800

    Web Dev Day 24 - Node.js - created basic HTTP server with routing

--- 23.0 Node.js/index.js
+++ 23.0 Node.js/index.js
@@ -1,7 +1,29 @@
-console.log("Hello from Node!")
+// console.log("Hello from Node!")
 
-let name = "Khyle";
+// let name = "Khyle";
 
-for (i = 0; i < 3; i++) {
-    console.log(name)
-}
+// for (i = 0; i < 3; i++) {
+//     console.log(name)
+// }
+
+import http from "http";
+import url from "url";
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+
+    if (parsedUrl.pathname === "/" && req.method === "GET") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>Wecome to the homepage! </h1>");
+    } else if (parsedUrl.pathname === "/about" && req.method === "GET") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>About us</h1>");
+    } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>Page not found</h1>")
+    }
+})
+
+server.listen(3000, () => {
+    console.log("Server running at http://localhost:3000/")
+})
