c00233dc0c54ff11725dc8e28d3c696e11253cef
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2026-08-16 11:47:56 +0800

    Web Dev Day 29 - Node.js - Passing Data with EJS

---
+++ 25.2 Passing Data/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import ejs from "ejs";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+
+app.post("/submit", (req, res) => {
+    let fName = req.body["fName"];
+    let lName = req.body["lName"];
+    let fullName = fName + lName;
+    let lengthOfName = fullName.length;
+    fullName = fName + " " + lName;
+
+    res.render("index.ejs", {
+        fullName: fullName,
+        lengthOfName: lengthOfName
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Server is running at port: ${port}.`);
+})
