f5c313fed27671f06328781ea61b94eedaac0ce6
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2026-08-20 16:36:31 +0800

    Web Dev Day 33 - Node.js - Higher or Lower Game Project completed

--- 27.1 HighLow Game Project/index.js
+++ 27.1 HighLow Game Project/index.js
@@ -1,29 +1,82 @@
-import express from "express"
+import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let currentNumber = 0
-let nextNumber = 0
+app.use((req, res, next) => {
+    res.set("Cache-Control", "no-store");
+    next();
+});
+
+let currentNumber = null;
+let nextNumber = null;
+let score = 0;
+let life = 3;
+
+function generateNumber() {
+    return Math.floor(Math.random() * 100);
+}
+
+function checkAnswer(nextNumber, currentNumber) {
+    if (nextNumber > currentNumber) {
+        return "higher";
+    } else if (nextNumber < currentNumber) {
+        return "lower";
+    } else {
+        return "tie";
+    }
+}
+
+function resetGame() {
+    score = 0
+    life = 3
+    currentNumber = generateNumber();
+}
 
 app.get("/", (req, res) => {
-    currentNumber = Math.floor(Math.random() * 100);
-
-    res.render("index.ejs", {currentNumber: currentNumber});
-})
-
-app.post("/guess", (req, res) => {
-    nextNumber = Math.floor(Math.random() * 100);
-    console.log(req.body.guess)
+    resetGame();
     res.render("index.ejs", {
         currentNumber: currentNumber,
-        nextNumber: nextNumber
-    })
-})
+        score: score,
+        life: life
+    });
+});
+
+app.post("/guess", (req, res) => {
+
+    if (!req.body?.guess) {
+        return res.redirect("/");
+    }
+
+    nextNumber = generateNumber();
+    let answer = checkAnswer(nextNumber, currentNumber)
+    const userChoice = req.body.guess;
+
+    if (userChoice === answer) {
+        score++;
+    } else {
+        life--;
+    }
+
+    if (life <= 0) {
+        return res.send(`
+            <h1>Game Over</h1>
+            <p>Score: ${score}</p>
+        `);
+    }
+
+    res.render("index.ejs", {
+        currentNumber: currentNumber,
+        nextNumber: nextNumber,
+        score: score,
+        life: life
+    });
+    currentNumber = nextNumber;
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-})
+});
