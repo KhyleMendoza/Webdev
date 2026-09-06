699af13a808fcde6f785cf3243db4264c1f6e776
KhyleMendoza <m.mendoza30743@mcc.edu.ph>
2026-09-06 13:13:35 +0800

    Web Dev Day 50 - Node.js - PostgreSQL - World Capital Quiz

--- 33.1 World Capital Quiz/index.js
+++ 33.1 World Capital Quiz/index.js
@@ -1,14 +1,36 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import "dotenv/config"
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
+
+db.connect();
 
 let quiz = [
   { country: "France", capital: "Paris" },
   { country: "United Kingdom", capital: "London" },
   { country: "United States of America", capital: "New York" },
 ];
+
+db.query("SELECT * FROM capitals", (err, res) =>  {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+  }
+
+  db.end();
+});
 
 let totalCorrect = 0;
 
